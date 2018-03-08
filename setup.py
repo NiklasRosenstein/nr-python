25fb8450cdf583d71971930b778c5773ce34ae00
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-03-08 13:22:11 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
+import os
 import sys
 from setuptools import find_packages, setup
 
+def readme():
+  if os.path.isfile('README.md') and any('dist' in x for x in sys.argv[1:]):
+    if os.system('pandoc -s README.md -o README.rst') != 0:
+      print('-----------------------------------------------------------------')
+      print('WARNING: README.rst could not be generated, pandoc command failed')
+      print('-----------------------------------------------------------------')
+      if sys.stdout.isatty():
+        input("Enter to continue... ")
+    else:
+      print("Generated README.rst with Pandoc")
+
+  if os.path.isfile('README.rst'):
+    with open('README.rst') as fp:
+      return fp.read()
+  return ''
+
 setup(
   name='bundler',
+  version='0.0.1',
+  license='MIT',
+  url='https://github.com/NiklasRosenstein/py-bundler',
   author='Niklas Rosenstein',
   author_email='rosensteinniklas@gmail.com',
   description='Bundle the modules of a Python application. (WIP)',
-  version='0.0.1',
+  long_description=readme(),
   packages=find_packages(),
   entry_points=dict(
     console_scripts=[
