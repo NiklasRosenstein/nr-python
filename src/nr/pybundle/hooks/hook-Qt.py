62039a3b5b14cbf3198aaeac125a6879f3810163
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-07-09 18:07:09 +0200

    hook-Qt.py now removes the PyQt5 bin directory from PATH, allowing proper resolution of VCRUNTIME140.dll outside of the Qt/bin/ directory

--- src/nr/pybundle/hooks/hook-Qt.py
+++ src/nr/pybundle/hooks/hook-Qt.py
@@ -1,14 +1,20 @@
 
 import Qt
-import logging
-
-logger = logging.getLogger(__name__)
+import os
 
 def examine(finder, module, result):
+  if module.name != 'Qt': return
   result.imports.append('Qt')
   result.imports.append(Qt.__binding__)
-  # TODO: Import the whole binding's package, not just the root module.
-  #result.imports += data.finder.iter_package_modules(module)
+
+  if Qt.__binding__ == 'PyQt5':
+    # We imported the 'Qt' module, which in turn imported 'PyQt5'.
+    # PyQt5 adds its bin/ directory the PATH, preventing proper
+    # resolution of required DLLs later with nr.pybundle.
+    import PyQt5
+    paths = os.environ['PATH'].split(os.pathsep)
+    paths.remove(os.path.dirname(PyQt5.__file__) + '\\Qt\\bin')
+    os.environ['PATH'] = os.pathsep.join(paths)
 
 
 def finalize(finder):
