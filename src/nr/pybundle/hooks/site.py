07034d76d11106945bcdc039c59ffdbe82c3c01f
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-07-11 02:48:44 +0200

    #10 update site.py with builtins -- note that help() won't work in bundled applications unless the pydoc module is available

--- src/nr/pybundle/hooks/site.py
+++ src/nr/pybundle/hooks/site.py
@@ -2,4 +2,14 @@
 import sys
 sys.frozen = True
 
-# TODO: Set up zipimports if there is a `libs.zip` file in the package root.
+PREFIXES = []
+ENABLE_USER_SITE = False
+USER_SITE = ''
+USER_BASE = None
+
+import builtins
+import _sitebuiltins
+
+builtins.quit = _sitebuiltins.Quitter('quit', 'CTRL-Z plus Return')
+builtins.exit = _sitebuiltins.Quitter('exit', 'CTRL-D (i.e. EOF)')
+builtins.help = _sitebuiltins._Helper()
