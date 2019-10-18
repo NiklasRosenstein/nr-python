c62995f1f2f8b4a4d5a5a06d38c45131d0011bba
Niklas Rosenstein <rosensteinniklas@gmail.com>
2019-10-18 23:12:58 +0200

    add nr.types.struct.contrib.config

---
+++ src/tests/test_struct_contrib_config.py
@@ -0,0 +1,42 @@
+
+import textwrap
+import yaml
+
+from nr.types.struct.contrib.config import Preprocessor, preprocess
+
+def test_preprocessor():
+  p = Preprocessor({'$serviceRoot': '/opt/app'})
+
+  assert p('{{$serviceRoot}}') == '/opt/app'
+
+  assert p({'config': {'directories': {'data': '{{$serviceRoot}}/data'}}}) == \
+    {'config': {'directories': {'data': '/opt/app/data'}}}
+
+
+def test_preprocessor_flat_update():
+  preprocessor = Preprocessor()
+
+  preprocessor.flat_update({'directory': {'data': '/opt/app/data'}})
+  assert preprocessor('{{directory.data}}') == '/opt/app/data'
+
+  preprocessor.flat_update({'key': [{'value': 'foo'}]})
+  print(preprocessor)
+  assert preprocessor('{{key[0].value}}') == 'foo'
+
+
+def test_preprocessor_e2e():
+  yaml_code = textwrap.dedent('''
+    config:
+      directories:
+        data: '{{$serviceRoot}}/data'
+    runtime:
+      media:
+        path: '{{directories.data}}/media'
+  ''')
+
+  data = preprocess(
+    yaml.safe_load(yaml_code),
+    init_variables={'$serviceRoot': '/opt/app'}
+  )
+
+  assert data == {'runtime': {'media': {'path': '/opt/app/data/media'}}}
