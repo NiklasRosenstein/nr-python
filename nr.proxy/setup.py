9467e5df16aa9ca791268f680568fe46eee01037
Niklas Rosenstein <rosensteinniklas@gmail.com>
2019-11-11 02:01:42 +0100

    feat(nr.proxy): Move nr.types.proxy to nr.proxy

---
+++ nr.proxy/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+  name='nr.proxy',
+  version='0.9.0.dev0',
+  packages=find_packages('src', exclude=['test*']),
+  package_dir={'': 'src'},
+  install_requires=['six']
+)
