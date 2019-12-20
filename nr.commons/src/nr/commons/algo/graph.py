8e9ce77a50adb7b48676bd80fd74c2fd07b6a63d
Niklas Rosenstein <rosensteinniklas@gmail.com>
2019-12-20 15:26:03 +0100

    feat(nr.commons): Add nr.commons.algo package with graph module and toposort() function

---
+++ nr.commons/src/nr/commons/algo/graph.py
@@ -0,0 +1,59 @@
+# -*- coding: utf8 -*-
+# Copyright (c) 2019 Niklas Rosenstein
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+class CyclicGraphError(Exception):
+  pass
+
+
+def toposort(nodes, get_dependencies, get_dependents=None):
+  # type: (List[Any], Callable[List[Any], [Any]]) -> Iterable[Any]
+  """ Performs topological sorting to the specified set of *nodes* and returns
+  a generator yielding the values of *nodes* in sorted order. If the dependents
+  of a node are know, *get_dependents* can be specified, otherwise it is built
+  by reversing *get_dependencies*. """
+
+  if get_dependents is None:
+    _dependents = {}
+    for node in nodes:
+      for dependent in get_dependencies(node):
+        _dependents.setdefault(dependent, []).append(node)
+    def get_dependents(node):
+      return _dependents.get(node, [])
+
+  visit = []
+  num_edges = 0
+  for node in nodes:
+    deps =  get_dependencies(node)
+    num_edges += len(deps)
+    if not deps:
+      visit.append(node)
+
+  seen = set()
+  while visit:
+    node = visit.pop(0)
+    yield node
+    for dependent in get_dependents(node):
+      seen.add((node, dependent))
+      if all((x, dependent) in seen for x in get_dependencies(dependent)):
+        visit.append(dependent)
+
+  if len(seen) != num_edges:
+    raise CyclicGraphError(len(seen), num_edges)
---
+++ nr.commons/src/test/nr/commons/algorithm/test_graph.py
@@ -0,0 +1,28 @@
+
+from nr.commons.algo.graph import CyclicGraphError, toposort
+import pytest
+
+
+def test_toposort():
+  nodes = {
+    'a': [],
+    'b': ['a'],
+    'c': ['b'],
+    'd': [],
+    'e': ['d', 'b'],
+    'f': ['c', 'e']
+  }
+
+  order = list(toposort(sorted(nodes), nodes.get))
+  assert order == ['a', 'd', 'b', 'c', 'e', 'f']
+  order = list(toposort(sorted(nodes, reverse=True), nodes.get))
+  assert order == ['d', 'a', 'b', 'e', 'c', 'f']
+
+
+def test_toposort_cycle():
+  nodes = {
+    'a': ['b'],
+    'b': ['a']
+  }
+  with pytest.raises(CyclicGraphError):
+    list(toposort(nodes, nodes.get))
