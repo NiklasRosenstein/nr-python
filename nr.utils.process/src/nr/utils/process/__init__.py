68c98665b2a942b17eb6ea88a89b52465528ee52
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-03-01 00:31:14 +0100

    (nr.utils.process) bump version to 0.0.2

--- nr.utils.process/src/nr/utils/process/__init__.py
+++ nr.utils.process/src/nr/utils/process/__init__.py
@@ -23,7 +23,7 @@ import os
 import sys
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 def process_exists(pid):  # type: (int) -> bool
