fee8587294651d5ea9a393d6dde1ca39a3066608
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-03-19 18:02:45 +0100

    update(nr.databind.json): Update dependency on nr.parsing.date

--- nr.databind.json/setup.py
+++ nr.databind.json/setup.py
@@ -10,7 +10,7 @@ with io.open('src/nr/databind/json/__init__.py', encoding='utf8') as fp:
 with io.open('README.md', encoding='utf8') as fp:
   long_description = fp.read()
 
-requirements = ['nr.collections >=0.0.1,<0.1.0', 'nr.databind.core >=0.0.1,<0.1.0', 'nr.interface >=0.0.1,<0.1.0', 'nr.parsing.date >=0.0.2,<0.1.0', 'nr.pylang.utils >=0.0.1,<0.1.0']
+requirements = ['nr.collections >=0.0.1,<0.1.0', 'nr.databind.core >=0.0.1,<0.1.0', 'nr.interface >=0.0.1,<0.1.0', 'nr.parsing.date >=0.1.0,<0.2.0', 'nr.pylang.utils >=0.0.1,<0.1.0']
 extras_require = {}
 extras_require['test'] = ['pytest', 'PyYAML']
 tests_require = []
