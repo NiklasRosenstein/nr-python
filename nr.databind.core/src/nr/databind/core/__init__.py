47762af7843d1bf17a6ebb42db99722deb7be918
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-04-18 05:38:20 +0200

    (nr.databind.core) bump version to 0.0.9

--- nr.databind.core/src/nr/databind/core/__init__.py
+++ nr.databind.core/src/nr/databind/core/__init__.py
@@ -20,7 +20,7 @@
 # IN THE SOFTWARE.
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 
 from nr.pylang.utils import NotSet
 import inspect
