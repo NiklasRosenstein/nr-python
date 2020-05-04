9feca1d4a88f9d536d77685436d993d08516e207
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-05-04 17:55:05 +0200

    fix(nr.pylang.bundle): (hook-sysconfig.py) sysconfig.get_sysconfigdata_name() does not always expect an argument (eg. on OSX)

--- nr.pylang.bundle/src/nr/pylang/bundle/hooks/hook-sysconfig.py
+++ nr.pylang.bundle/src/nr/pylang/bundle/hooks/hook-sysconfig.py
@@ -1,11 +1,20 @@
 
 import os
 
+try:
+  from inspect import getfullargspec as getargspec
+except ImportError:
+  from inspect import getargspec
+
 
 def extend_imports(module):
   if module.name == 'sysconfig' and os.name == 'posix':
     import sysconfig
     if hasattr(sysconfig, '_get_sysconfigdata_name'):
-      module.imports.append(sysconfig._get_sysconfigdata_name(True))
+      args = []
+      if getargspec(sysconfig._get_sysconfigdata_name).args:
+        args = [True]
+      sysconfig_name = sysconfig._get_sysconfigdata_name(*args)
     else:
-      module.imports.append('_sysconfigdata')
+      sysconfig_name = '_sysconfigdata'
+    module.imports.append(sysconfig_name)
