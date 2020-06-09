3ddbfd56625517f5f5b90b0037a58c28102d4319
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-06-09 23:39:22 +0200

    docs(nr.utils.wsgi): update file headers

--- nr.utils.wsgi/src/nr/utils/wsgi/__init__.py
+++ nr.utils.wsgi/src/nr/utils/wsgi/__init__.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2018 Niklas Rosenstein
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 Niklas Rosenstein
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
