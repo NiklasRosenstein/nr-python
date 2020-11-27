46b87cb14d628cfa815c4b013b5b2833f62a4651
Niklas Rosenstein <rosensteinniklas@gmail.com>
2020-11-27 02:57:40 +0100

    (nr.parsing.date) bump version to 0.4.3

--- nr.parsing.date/setup.py
+++ nr.parsing.date/setup.py
@@ -34,7 +34,7 @@ test_requirements = [
 
 setuptools.setup(
   name = 'nr.parsing.date',
-  version = '0.4.2',
+  version = '0.4.3',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   description = 'A simple and fast date parsing library. Uses dateutil for timezone offset support.',
