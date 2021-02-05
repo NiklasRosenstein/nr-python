9700f7c556a30b9be2dba47d7d1175ec71404109
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-02-05 23:28:03 +0000

    change(nr.utils.git): Add `Git.check_call()` and `Git.check_output()`, change `Git.clone()` (removed `to_directory` argument and returns `None`), add `Git.init()` and `Git.rename_branch()`, fix error with `Git.tag()` not passing the directory

--- nr.utils.git/src/nr/utils/git/__init__.py
+++ nr.utils.git/src/nr/utils/git/__init__.py
@@ -35,11 +35,19 @@ RefWithSha = collections.namedtuple('RefWithSha', 'ref,sha')
 class Git:
 
   def __init__(self, cwd: str = None):
-    self.cwd = cwd
+    self.cwd = cwd or os.getcwd()
+
+  def check_call(self, command: t.List[str], stdout: t.Optional[int] = None) -> None:
+    sp.check_call(command, cwd=self.cwd, stdout=stdout)
+
+  def check_output(self, command: t.List[str], stderr: t.Optional[int] = None) -> bytes:
+    return sp.check_output(command, cwd=self.cwd, stderr=stderr)
+
+  def init(self) -> None:
+    self.check_call(['git', 'init', '.'])
 
   def clone(
       self,
-      to_directory: str,
       clone_url: str,
       branch: str = None,
       depth: int = None,
@@ -47,7 +55,7 @@ class Git:
       username: str = None,
       password: str = None,
       quiet: bool = False,
-  ) -> 'Git':
+  ) -> None:
     """
     Clone a Git repository to the *to_directory* from *clone_url*. If a relative path is
     specified in *to_directory*, it will be treated relative to the #Git.cwd path.
@@ -60,7 +68,7 @@ class Git:
       auth = ':'.join(t.cast(t.List[str], filter(bool, [username, password])))
       clone_url = schema + '://' + auth + '@' + remainder
 
-    command = ['git', 'clone', clone_url, to_directory]
+    command = ['git', 'clone', clone_url, self.cwd]
     if branch:
       command += ['-b', branch]
     if depth:
@@ -70,8 +78,7 @@ class Git:
     if quiet:
       command += ['-q']
 
-    sp.check_call(command, cwd=self.cwd)
-    return Git(to_directory)
+    sp.check_call(command)
 
   def add(self, files: t.List[str]) -> None:
     """
@@ -79,7 +86,7 @@ class Git:
     """
 
     command = ['git', 'add', '--'] + files
-    sp.check_call(command, cwd=self.cwd)
+    self.check_call(command)
 
   def get_branches(self) -> t.List[Branch]:
     """
@@ -88,7 +95,7 @@ class Git:
 
     command = ['git', 'branch']
     results = []
-    for line in sp.check_output(command, cwd=self.cwd).decode().splitlines():
+    for line in self.check_output(command).decode().splitlines():
       current = False
       if line.startswith('*'):
         line = line[1:]
@@ -118,7 +125,7 @@ class Git:
   def get_remote_refs(self, remote: str) -> t.List[RefWithSha]:
     result = []
     command = ['git', 'ls-remote', '--heads', 'origin']
-    for line in sp.check_output(command, cwd=self.cwd).decode().splitlines():
+    for line in self.check_output(command).decode().splitlines():
       sha, ref = line.split()
       result.append(RefWithSha(ref, sha))
     return result
@@ -126,6 +133,9 @@ class Git:
   def get_remote_branch_names(self, remote: str) -> t.List[str]:
     refs = self.get_remote_refs(remote)
     return [x.ref[11:] for x in refs if x.ref.startswith('refs/heads/')]
+
+  def rename_branch(self, current: str, new: str) -> None:
+    self.check_call(['git', 'branch', '-m', current, new])
 
   def push(self, remote: str, *refs, force: bool = False) -> None:
     """
@@ -135,7 +145,7 @@ class Git:
     command = ['git', 'push', remote] + list(refs)
     if force:
       command.insert(2, '-f')
-    sp.check_call(command, cwd=self.cwd)
+    self.check_call(command)
 
   def pull(self, remote: str = None, branch: str = None, quiet: bool = False):
     """
@@ -150,14 +160,14 @@ class Git:
     elif remote or branch:
       raise ValueError('remote and branch arguments can only be specified together')
 
-    sp.check_call(command, cwd=self.cwd)
+    self.check_call(command)
 
   def porcelain(self) -> t.Iterable[FileStatus]:
     """
     Returns the file status for the working tree.
     """
 
-    for line in sp.check_output(['git', 'status', '--porcelain'], cwd=self.cwd).decode().splitlines():
+    for line in self.check_output(['git', 'status', '--porcelain']).decode().splitlines():
       mode, filename = line.strip().partition(' ')[::2]
       yield FileStatus(mode, filename)
 
@@ -169,7 +179,7 @@ class Git:
     command = ['git', 'commit', '-m', message]
     if allow_empty:
       command.append('--allow-empty')
-    sp.check_call(command, cwd=self.cwd)
+    self.check_call(command)
 
   def tag(self, tag_name: str, force: bool = False) -> None:
     """
@@ -177,7 +187,7 @@ class Git:
     """
 
     command = ['git', 'tag', tag_name] + (['-f'] if force else [])
-    sp.check_call(command)
+    self.check_call(command)
 
   def rev_parse(self, rev: str) -> t.Optional[str]:
     """
@@ -186,7 +196,7 @@ class Git:
 
     command = ['git', 'rev-parse', rev]
     try:
-      return sp.check_output(command, stderr=sp.STDOUT, cwd=self.cwd).decode().strip()
+      return self.check_output(command, stderr=sp.STDOUT).decode().strip()
     except sp.CalledProcessError:
       return None
 
@@ -199,7 +209,7 @@ class Git:
     if path:
       command += ['--', path]
     try:
-      revlist = sp.check_output(command, stderr=sp.STDOUT, cwd=self.cwd).decode().strip().split('\n')
+      revlist = self.check_output(command, stderr=sp.STDOUT).decode().strip().split('\n')
     except sp.CalledProcessError:
       return []
     if revlist == ['']:
@@ -212,7 +222,7 @@ class Git:
     """
 
     try:
-      sp.check_call(['git', 'diff', '--exit-code'], stdout=sp.PIPE, cwd=self.cwd)
+      self.check_call(['git', 'diff', '--exit-code'], stdout=sp.PIPE)
       return False
     except sp.CalledProcessError as exc:
       if exc.returncode == 1:
@@ -227,7 +237,7 @@ class Git:
     command = ['git', 'checkout', '-b', name]
     if orphan:
       command += ['--orphan']
-    sp.check_call(command, cwd=self.cwd)
+    self.check_call(command)
 
   def checkout(self, ref: str = None, files: t.List[str] = None, quiet: bool = False):
     """
@@ -241,7 +251,7 @@ class Git:
       command += ['-q']
     if files:
       command += ['--'] + files
-    sp.check_call(command, cwd=self.cwd)
+    self.check_call(command)
 
   def reset(self, ref: str = None, files: t.List[str] = None, hard: bool = False, quiet: bool = False):
     """
@@ -255,14 +265,14 @@ class Git:
       command += ['-q']
     if files:
       command += ['--'] + files
-    sp.check_call(command, cwd=self.cwd)
+    self.check_call(command)
 
   def get_commit_message(self, rev: str) -> str:
     """
     Returns the commit message of the specified *rev*.
     """
 
-    return sp.check_output(['git', 'log', '-1', rev, '--pretty=%B']).decode()
+    return self.check_output(['git', 'log', '-1', rev, '--pretty=%B']).decode()
 
   def get_diff(self, files: t.List[str] = None, cached: bool = False):
     command = ['git', '--no-pager', 'diff', '--color=never']
@@ -270,7 +280,7 @@ class Git:
       command += ['--cached']
     if files is not None:
       command += ['--'] + files
-    return sp.check_output(command, cwd=self.cwd).decode()
+    return self.check_output(command).decode()
 
   def describe(self,
     all: bool = False,
@@ -290,7 +300,7 @@ class Git:
       command.append(commitish)
 
     try:
-      return sp.check_output(command, stderr=sp.DEVNULL).decode().strip()
+      return self.check_output(command, stderr=sp.DEVNULL).decode().strip()
     except FileNotFoundError:
       raise
     except sp.CalledProcessError:
