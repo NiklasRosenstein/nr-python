93c0a4a18a1390181397773eeae27da60a9dff9b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-03-20 02:43:01 +0100

    (nr.parsing.date) bump version to 0.6.0

--- nr.parsing.date/src/nr/parsing/date.py
+++ nr.parsing.date/src/nr/parsing/date.py
@@ -39,7 +39,7 @@ if t.TYPE_CHECKING:
   import dateutil.relativedelta
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 
 
 class timezone(tzinfo):
