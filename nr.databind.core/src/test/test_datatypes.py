624517f94e2a652651782013be4bc8d788f3c4a6
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-05-18 21:57:19 +0200

    remove nr.databind.json and nr.databind.core as they are fully deprecated

--- nr.databind.core/src/test/test_datatypes.py
+++
@@ -1,8 +0,0 @@
-
-from nr.databind.core.datatypes import *
-from typing import Union
-
-
-def test_union_to_multitype():
-  datatype = translate_type_def(Union[str, int])
-  assert datatype == MultiType([str, int])
