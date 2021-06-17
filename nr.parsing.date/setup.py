d61a5b8040dd596ee42d024c3c757c3db2a7767f
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-06-17 02:14:02 +0200

    (nr.parsing.date) bump version to 1.0.1

--- nr.parsing.date/setup.py
+++ nr.parsing.date/setup.py
@@ -37,7 +37,7 @@ test_requirements = [
 
 setuptools.setup(
   name = 'nr.parsing.date',
-  version = '1.0.0',
+  version = '1.0.1',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   description = 'A fast, regular-expression based library for parsing dates, plus support for ISO 8601 durations.',
