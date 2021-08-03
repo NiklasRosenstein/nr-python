88ecaa2db88bfcc003878f75fcb8355903b058d2
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-08-04 00:29:49 +0200

    (nr.utils.git) bump version to 0.4.0

--- nr.utils.git/src/nr/utils/git/__init__.py
+++ nr.utils.git/src/nr/utils/git/__init__.py
@@ -26,7 +26,7 @@ import subprocess as sp
 import typing as t
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 
 class Branch(t.NamedTuple):
