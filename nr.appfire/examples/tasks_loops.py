bdf3656e64ab90aa345d38b0133a74e9af7d1b55
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-09-07 02:37:47 +0100

    add `nr.appfire.tasks` package

--- nr.appfire/examples/tasks_loops.py
+++ nr.appfire/examples/tasks_loops.py
@@ -1,16 +1,3 @@
-# nr.appfire
-
-Appfire is a toolkit that provides utilities for quickly building configurable microservices.
-
-## Components
-
-### `nr.appfire.tasks`
-
-This package provides an easy-to-use framework for managing background tasks in a Python application.
-
-__Example__
-
-```py
 from dataclasses import dataclass
 from nr.appfire.tasks import Task, TaskManager
 
@@ -31,8 +18,3 @@ class MyTask(Task):
 manager = TaskManager('MyApp')
 manager.queue(MyTask(10))
 manager.idlejoin()
-```
-
----
-
-<p align="center">Copyright &copy; 2021 Niklas Rosenstein</p>
