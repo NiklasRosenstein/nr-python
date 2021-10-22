368b30436e29aa2aab82afe97a76fa00aad1bafd
Niklas Rosenstein <rosensteinniklas@gmail.com>
2021-10-23 00:16:49 +0200

    feature(nr.functional): add `coalesce()` function

--- nr.functional/src/nr/functional/__init__.py
+++ nr.functional/src/nr/functional/__init__.py
@@ -28,3 +28,26 @@ class flatmap(t.Generic[T, R]):
     if value is not None:
       return self.func(value)
     return None
+
+
+@t.overload
+def coalesce(value: t.Optional[T], fallback: T) -> T: ...
+
+@t.overload
+def coalesce(value: t.Optional[T], *values: t.Optional[T]) -> t.Optional[T]: ...
+
+@t.overload
+def coalesce(value: t.Optional[T], *values: t.Optional[T], fallback: T) -> T: ...
+
+def coalesce(value: t.Optional[T], *values: t.Optional[T], fallback: t.Optional[T] = None) -> t.Optional[T]:
+  """
+  Returns the first value that is not `None`. If a not-None fallback is specified, the function is guaranteed
+  to return a not-Non value.
+  """
+
+  if value is not None:
+    return value
+  value = next((x for x in values if x is not None), None)
+  if value is not None:
+    return value
+  return fallback
